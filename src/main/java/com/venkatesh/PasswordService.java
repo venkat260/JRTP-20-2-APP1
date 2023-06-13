9dd1f2ce2b59ce948baff768ec0d7c9b71d8c239
venkat260 <chigiri.vennkatesh@gmail.com>
2023-06-13 13:55:42 +0530

    first commit

---
+++ src/main/java/com/venkatesh/PasswordService.java
@@ -0,0 +1,39 @@
+package com.venkatesh;
+
+import java.util.Base64;
+import java.util.Base64.Decoder;
+import java.util.Base64.Encoder;
+
+
+
+public class PasswordService {
+
+	public static String passwordEncoder(String txt) {
+
+		Encoder encoder = Base64.getEncoder();
+
+		String encodeToString = encoder.encodeToString(txt.getBytes());
+		return encodeToString;
+
+	}
+
+	public static String decoder(String encodeToString) {
+
+		Decoder decoder = Base64.getDecoder();
+		byte[] decode = decoder.decode(encodeToString);
+		return new String(decode);
+
+	}
+
+	public static void main(String[] args) {
+
+		String passwordEncoder = PasswordService.passwordEncoder("lavan");
+		System.out.println(passwordEncoder);
+
+		String decoder = PasswordService.decoder(passwordEncoder);
+
+		System.out.println(decoder);
+
+	}
+
+}
